ba025a805ed96cb1e8067db48d9167f2b6bd1636
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-20 14:36:28 +0700

    class mahasiswa

---
+++ memberinit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+private:
+    const int id;
+    string nama;
+    float nilai;
+public:
+    Mahasiswa(int pId, string pNama, float pNilai) : id(pId), nama(pNama), nilai(pNilai)
+    {
+
+    }
+
+    ~Mahasiswa() {
+        cout << "Id      = " << id << endl;
+        cout << "nama    = " << nama << endl;
+        cout << "nilai   = " << nilai << endl;
+
+    }    
+};
