d74dbeeea718e97b0d219e5a48de56425cc615b8
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-20 15:33:33 +0700

    using namespace

--- Agredasi/ibu.cpp
+++ Agredasi/ibu.cpp
@@ -1,22 +1,31 @@
 #include <iostream>
-#include <vector)
+#include <vector>
+using namespace std;
 
 #include "anak.H"
 #include "ibu.H"
 
 int main() {
-    ibu* varibu = new ibu("dini");
-    ibu* varibu = new ibu("novi");
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
     anak* varAnak1 = new anak("tono");
     anak* varAnak2 = new anak("rini");
     anak* varAnak3 = new anak("dewi");
 
     varibu->tambahAnak(varAnak1);
     varibu->tambahAnak(varAnak2);
-    varibu->tambahAnak(varAnak3);
-    varibu->tambahAnak(varAnak4);
+    varibu2->tambahAnak(varAnak3);
+    varibu2->tambahAnak(varAnak4);
 
     varibu->cetakAnak();
-    varibu->cetakAnak();
+    varibu2->cetakAnak();
+
+    delete varibu;
+    delete varibu2;
+    delete varAnak1;
+    delete varAnak2;
+    delete varAnak3;
+    delete varAnak4;
+
 
 }
