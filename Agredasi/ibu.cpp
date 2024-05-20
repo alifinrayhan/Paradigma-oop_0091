9803f89d7a2f4904b11b4d4f62d0044a6029be41
alifinrayhan@gmail.com <alifinrayhan@gmail.com>
2024-05-20 15:43:57 +0700

    class jantung

--- Agredasi/ibu.cpp
+++ Agredasi/ibu.cpp
@@ -5,27 +5,26 @@ using namespace std;
 #include "anak.H"
 #include "ibu.H"
 
-int main() {
-    ibu* varIbu = new ibu("dini");
-    ibu* varIbu2 = new ibu("novi");
-    anak* varAnak1 = new anak("tono");
-    anak* varAnak2 = new anak("rini");
-    anak* varAnak3 = new anak("dewi");
+int main()
+{
+    ibu *varIbu = new ibu("dini");
+    ibu *varIbu2 = new ibu("novi");
+    anak *varAnak1 = new anak("tono");
+    anak *varAnak2 = new anak("rini");
+    anak *varAnak3 = new anak("dewi");
 
-    varibu->tambahAnak(varAnak1);
-    varibu->tambahAnak(varAnak2);
-    varibu2->tambahAnak(varAnak3);
-    varibu2->tambahAnak(varAnak4);
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
 
-    varibu->cetakAnak();
-    varibu2->cetakAnak();
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
 
-    delete varibu;
-    delete varibu2;
+    delete varIbu;
+    delete varIbu2;
     delete varAnak1;
     delete varAnak2;
     delete varAnak3;
-    delete varAnak4;
-
-
+    
 }
